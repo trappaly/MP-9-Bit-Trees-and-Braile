ec0d0e06f6fc26ccd4dd50ddb604b9c178921276
trappaly <trappaly@grinnell.edu>
2024-04-24 22:29:45 -0500

    edited BitTree

--- src/BitTree.java
+++ src/BitTree.java
@@ -9,6 +9,7 @@ import java.io.InputStreamReader;
 
 /**
  * A bit tree that makes a bit tree with n levels
+ * 
  * @author Alyssa Trapp
  */
 public class BitTree {
@@ -34,215 +35,178 @@ public class BitTree {
   /**
    * Create a new bit tree based on the number of elements.
    */
-  public BitTree(int n){
+  public BitTree(int n) {
     this.size = n;
-    this.root = new BitTreeInteriorNode(null); 
+    this.root = new BitTreeInteriorNode(null);
   } // BitTree (int)
 
   // +---------+-----------------------------------------------------
   // | Methods |
   // +---------+
+  
 
 
-  public BitTreeNode setHelper (BitTreeNode node, String bits, int i, String value){
-    BitTreeInteriorNode innerNode = (BitTreeInteriorNode) node;
-    if (bits.length() == i){
-      //System.out.println("leaf" + new BitTreeLeaf(value));
-     return new BitTreeLeaf(value); 
+
+  public BitTreeNode setHelper(BitTreeNode node, String bits, int i, String value) {
+    if (node instanceof BitTreeLeaf){
+      return new BitTreeLeaf(value);
     }
     else {
-     if (bits.charAt(i) == '0'){
-      if (innerNode.left == null){
-      innerNode.left = new BitTreeInteriorNode(null); 
-      setHelper (innerNode.left, bits, i + 1, value);
-      }
-    }
-    else if (bits.charAt(i) == '1') {
-      if (innerNode.right == null){
-      innerNode.right = new BitTreeInteriorNode(null); 
-      innerNode.right = setHelper (innerNode.right, bits, i + 1, value);
-    }
-  }
-  //System.out.println("innerNode" + innerNode);
-    return innerNode;
-  }
-}
-
-
-   public void set (String bits, String value) throws Exception{
-  if (((!bits.contains("0")) || (!bits.contains("1"))) &&  bits.length() != size){
-      throw new Exception();
-    } // if
-    setHelper (root, bits, 0, value);
-   } // set (String, String)
-
-
-   /*public String getHelper (BitTreeNode node, String bits, int i) throws Exception{
-// use instanceOf
-    BitTreeInteriorNode innerNode = (BitTreeInteriorNode) node;
-    if (node == null) {
-      throw new Exception ("ran out of bits");
-    }
-    else {
-      if (bits.length() == i){
-      BitTreeLeaf leaf = (BitTreeLeaf) node;
-       return leaf.value;
-      }
-     else if (bits.charAt(i) == '0'){
-      return getHelper (innerNode.left, bits, i++);
-     }
-     else {
-      return getHelper (innerNode.right, bits, i++);
-     }
-    }
-  }*/
-
-
-  public String getHelper (BitTreeNode node, String bits, int i) throws Exception{
-    // use instanceOf
-        BitTreeInteriorNode innerNode = (BitTreeInteriorNode) node;
+      // i know he said not to do this, but i don't understand how to access the left and right value otherwise
+      BitTreeInteriorNode innerNode = (BitTreeInteriorNode) node;
+      if (bits.charAt(i) == '0') {
         if (node == null) {
-          throw new Exception ("ran out of bits");
+          innerNode.left = new BitTreeInteriorNode(null);
+          setHelper(innerNode.left, bits, i ++, value);
         }
-        else {
-          if (node instanceof BitTreeLeaf){
-            return "";
-          }
-          else if (node instanceof BitTreeInteriorNode){
-        if (bits.charAt(i) == '0'){
-          return getHelper (innerNode.left, bits, i++);
-         }
-         else {
-          return getHelper (innerNode.right, bits, i++);
-         }
+      } else if (bits.charAt(i) == '1') {
+        if (node == null) {
+          innerNode.right = new BitTreeInteriorNode(null);
+          setHelper(innerNode.right, bits, i ++, value);
         }
-  
       }
+      // System.out.println("innerNode" + innerNode);
+      return innerNode;
     }
+  }
 
-
-   public String get (String bits) throws Exception {
-    if (bits.length() != 6){
+  public void set(String bits, String value) throws Exception {
+    if (((!bits.contains("0")) || (!bits.contains("1"))) && bits.length() != size) {
       throw new Exception();
-    }
-    return getHelper(root, bits, 0);
-   } // get (String)
-
-
- /*  BitTreeInteriorNode innerNode = (BitTreeInteriorNode) node;
-    BitTreeLeaf leaf = (BitTreeLeaf) node;
-    if (node != null) {
-    if (bits.length() == i) {
-       return leaf.value;
-    }
-    else {
-      i++;
-    if (bits.charAt(i) == '0'){
-      return getHelper (innerNode.left, bits, i);
-      }
-    else if (bits.charAt(i) == '1'){  
-        return getHelper (innerNode.right, bits, i);
-        }
-    }
-  }
-  return leaf.value;
-  //throw new Exception();
-} */
-
-
-  /**
-   * Dump a portion of the tree to some output location.
-  */
-
-  // had string ""
-  
-  void dumpHelper(PrintWriter pen, BitTreeNode node, String bits, int i) {
-    BitTreeInteriorNode innerNode = (BitTreeInteriorNode) node;
-    //pen.println(bits + "," + node.get(bits));
-    if ((innerNode.right == null) && (innerNode.left == null)) {
-      try {
-        pen.println(get(bits));
-        //"," + get(bits));
-        return;
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    } else {
-        pen.println((bits.charAt(i)));
-        if (bits.charAt(i) == '0'){
-          dumpHelper(pen, innerNode.left, bits, i++);
-        }
-        else if (bits.charAt(i) == '1'){
-          dumpHelper(pen, innerNode.right, bits, i++);
-        }
-      }
     } // if
-  // if has children
+    setHelper(root, bits, 0, value);
+  } // set (String, String)
 
-
-
-
-  public void dump(PrintWriter pen) {
-    dumpHelper (pen, root, "", 0);
-  } // dump(PrintWriter)
-}
-
-
-
-
-   /*  Scanner scanner = new Scanner (source);
-    try {
-    while (scanner.hasNext()){
-      String newline =  scanner.nextLine();
-      String[] arr = newline.split("", 2);
-      set (arr[0], arr[1]);
-   } 
-  } catch (Exception e){
-  }
-  scan
-  */ 
-
-
-/* public void load (InputStream source){
-    BufferedReader scanner = new BufferedReader(new InputStreamReader(source));
-    String newline = reader.readLine();
-    while (newline  != null){
-        String[] arr = newline.split("", 2);
-        this.set (arr[0], arr[1]);
-        newline = reader.readLine();
+  // node 
+    public String getHelper(BitTreeNode node, String bits, int i) throws Exception {
+    BitTreeLeaf leaf = (BitTreeLeaf) node;
+    if (node instanceof BitTreeInteriorNode) {
+    // use instanceOf
+    BitTreeInteriorNode innerNode = (BitTreeInteriorNode) node;
+    //if (node == null) {
+      //throw new Exception("ran out of bits");
+    //} else {
+      if ((innerNode.left == null) && (innerNode.right == null)) {
+        //BitTreeLeaf leaf = (BitTreeLeaf) node;
+        return leaf.getLeaf();
+      } else if (bits.charAt(i) == '0') {
+        return getHelper(innerNode.left, bits, i++);
+      } else {
+        return getHelper(innerNode.right, bits, i++);
+      }
     }
-    */
-   
+    else if (node instanceof BitTreeLeaf){
+      return leaf.value;
+    }
+    return "I don't know what to return here";
+  }
+
+  public String get(String bits) throws Exception {
+    if (((!bits.contains("0")) || (!bits.contains("1"))) && bits.length() != size) {
+      throw new Exception();
+    } // if
+    return getHelper(root, bits, 0);
+  } // get (String)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-  // +---------+-----------------------------------------------------
-  // | Helpers |
-  // +---------+
+  /*
+   * BitTreeInteriorNode innerNode = (BitTreeInteriorNode) node;
+   * BitTreeLeaf leaf = (BitTreeLeaf) node;
+   * if (node != null) {
+   * if (bits.length() == i) {
+   * return leaf.value;
+   * }
+   * else {
+   * i++;
+   * if (bits.charAt(i) == '0'){
+   * return getHelper (innerNode.left, bits, i);
+   * }
+   * else if (bits.charAt(i) == '1'){
+   * return getHelper (innerNode.right, bits, i);
+   * }
+   * }
+   * }
+   * return leaf.value;
+   * //throw new Exception();
+   * }
+   */
 
   /**
    * Dump a portion of the tree to some output location.
    */
- /*  void dump(PrintWriter pen, BitTreeNode<T> node, String indent) {
-    if (node == null) {
-      pen.println(indent + "<>");
-    } else {
-      pen.println(indent + node.value);
-      if ((node.left != null) || (node.right != null)) {
-        dump(pen, node.left, indent + "  ");
-        dump(pen, node.right, indent + "  ");
-      } // if has children
-    } // else
-  } // dump
-*/
+
+  // had string ""
+  
+
+  void dumpHelper(PrintWriter pen, BitTreeNode node, String bits, int i) {
+    BitTreeInteriorNode innerNode = (BitTreeInteriorNode) node;
+    // pen.println(bits + "," + node.get(bits));
+    if (node instanceof BitTreeLeaf) {
+      try {
+        pen.println(get(bits));
+        // "," + get(bits));
+        return;
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    } 
+  else {
+      pen.println((bits.charAt(i)));
+      if (bits.charAt(i) == '0') {
+        dumpHelper(pen, innerNode.left, bits, i++);
+      } else if (bits.charAt(i) == '1') {
+        dumpHelper(pen, innerNode.right, bits, i++);
+      }
+    }
+  } // if
+  // if has children
+
+  public void dump(PrintWriter pen) {
+    dumpHelper(pen, root, "", 0);
+  } // dump(PrintWriter)
+}
+
+/*
+ * Scanner scanner = new Scanner (source);
+ * try {
+ * while (scanner.hasNext()){
+ * String newline = scanner.nextLine();
+ * String[] arr = newline.split("", 2);
+ * set (arr[0], arr[1]);
+ * }
+ * } catch (Exception e){
+ * }
+ * scan
+ */
+
+/*
+ * public void load (InputStream source){
+ * BufferedReader scanner = new BufferedReader(new InputStreamReader(source));
+ * String newline = reader.readLine();
+ * while (newline != null){
+ * String[] arr = newline.split("", 2);
+ * this.set (arr[0], arr[1]);
+ * newline = reader.readLine();
+ * }
+ */
+
+// +---------+-----------------------------------------------------
+// | Helpers |
+// +---------+
+
+/**
+ * Dump a portion of the tree to some output location.
+ */
+/*
+ * void dump(PrintWriter pen, BitTreeNode<T> node, String indent) {
+ * if (node == null) {
+ * pen.println(indent + "<>");
+ * } else {
+ * pen.println(indent + node.value);
+ * if ((node.left != null) || (node.right != null)) {
+ * dump(pen, node.left, indent + "  ");
+ * dump(pen, node.right, indent + "  ");
+ * } // if has children
+ * } // else
+ * } // dump
+ */
